b7f89e028d058a15cd5e54d6ab58c64b48b9c88f
Security Colleague <36559042+Sarthakkathuria@users.noreply.github.com>
2019-05-03 16:54:25 +0530

    Getting data update
    For getting data and storing data it in storing in message.txt

--- app.js
+++ app.js
@@ -1,49 +1,46 @@
 const http = require('http');
-const fs =require('fs');
+const fs = require('fs');
 
-
-const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
-    const method =req.method;
-    const url = req.url; 
-    //to check if url is / only
-    if (url=== '/'){
-        res.setHeader('Content-Type','text.html');
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  //to check if url is / only
+  if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>Mu first page</title></head>');
+    res.write('<head><title>Enter Message</title><head>');
     //redirect it to /message 
-    res.write('<body><form action="/message" method="POST"><input type="Text"><button type="submit"></button></form></body>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
     return res.end();
+  }
+  //if it is message 
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    //for getting data we use req.on for buffer
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    //for checking if the data is still available
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      //for creating new file 
+      fs.writeFileSync('message.txt', message);
 
-    }
-    //if it is message 
-    if (url ==='/message' && method==='POST'){
-        const body=[];
-        //for getting data we use req.on for buffer
-        req.on('data',(chunk) =>{
-            console.log(chunk);
-            body.push(chunk); 
-        });
-        //for checking if the data is still available
-        req.on('end',()=>{
-
-        })
-        fs.writeFileSync('message.text','DUMMY')
-         //for creating new file 
-        res,statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
-         
-    }
-
-    res.setHeader('Content-Type','text.html');
-    res.write('<html>');
-    res.write('<head><title>Mu first page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server</h1></body>');
-    res.write('</html>');
-
-    res.end();
+    });
+    
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
-server.listen(3000);
 
+server.listen(3000);
+ 
